5791b6a15be947af813a09df7c204eb8b8045016
lianghao <lhing17@163.com>
2020-12-29 19:25:44 +0800

    fix: 梳理excel转pdf的工具类。

--- src/main/java/cn/gsein/toolkit/excel/util/ExcelToPdfPdfBoxUtil.java
+++ src/main/java/cn/gsein/toolkit/excel/util/ExcelToPdfPdfBoxUtil.java
@@ -6,20 +6,13 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
-import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.awt.*;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.awt.Color;
+import java.io.*;
 import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Objects;
@@ -43,16 +36,18 @@ public final class ExcelToPdfPdfBoxUtil {
     private static final float POINTS_PER_MM = 2.8346457f;
     private static final float PAGE_WIDTH = 210 * POINTS_PER_MM;
     private static final float PAGE_HEIGHT = 297 * POINTS_PER_MM;
+    /**
+     * 内容所占宽度比例
+     */
+    private static final float CONTENT_WIDTH_PERCENT = 0.94f;
 
 
     /**
      * 获取字体
      */
-    private static org.apache.poi.ss.usermodel.Font getExcelFont(Workbook workbook, Cell cell, String excelName) {
-        if (excelName.endsWith(XLS)) {
-            return ((HSSFCell) cell).getCellStyle().getFont(workbook);
-        }
-        return ((XSSFCell) cell).getCellStyle().getFont();
+    private static Font getExcelFont(Workbook workbook, Cell cell) {
+        int fontIndex = cell.getCellStyle().getFontIndexAsInt();
+        return workbook.getFontAt(fontIndex);
     }
 
 
@@ -119,44 +114,15 @@ public final class ExcelToPdfPdfBoxUtil {
     private static int getMaxColRowNum(Sheet sheet) {
         int rowNum = 0;
         int maxCol = 0;
-        for (int r = sheet.getFirstRowNum(); r < sheet.getPhysicalNumberOfRows(); r++) {
+        for (int r = sheet.getFirstRowNum(); r <= sheet.getLastRowNum(); r++) {
             Row row = sheet.getRow(r);
-            if (row != null && maxCol < row.getPhysicalNumberOfCells()) {
-                maxCol = row.getPhysicalNumberOfCells();
+            if (row != null && maxCol < row.getLastCellNum() + 1) {
+                maxCol = row.getLastCellNum() + 1;
                 rowNum = r;
             }
         }
         return rowNum;
     }
-
-//    /**
-//     * excel垂直对齐方式映射到pdf对齐方式
-//     */
-//    private static int getVerticalAlignment(VerticalAlignment align) {
-//        switch (align) {
-//            case BOTTOM:
-//                return com.itextpdf.text.Element.ALIGN_BOTTOM;
-//            case TOP:
-//                return com.itextpdf.text.Element.ALIGN_TOP;
-//            default:
-//                return com.itextpdf.text.Element.ALIGN_MIDDLE;
-//        }
-//    }
-//
-//
-//    /**
-//     * excel水平对齐方式映射到pdf水平对齐方式
-//     */
-//    private static int getHorizontalAlignment(HorizontalAlignment align) {
-//        switch (align) {
-//            case RIGHT:
-//                return com.itextpdf.text.Element.ALIGN_RIGHT;
-//            case LEFT:
-//                return com.itextpdf.text.Element.ALIGN_LEFT;
-//            default:
-//                return com.itextpdf.text.Element.ALIGN_CENTER;
-//        }
-//    }
 
     /**
      * 格式化数字
@@ -170,9 +136,10 @@ public final class ExcelToPdfPdfBoxUtil {
      * 获取excel中每列宽度的占比
      */
     private static float[] getColWidth(Sheet sheet) {
+        // 获取最多列的行号
         int rowNum = getMaxColRowNum(sheet);
         Row row = sheet.getRow(rowNum);
-        int cellCount = row.getPhysicalNumberOfCells();
+        int cellCount = row.getLastCellNum() + 1;
         int[] colWidths = new int[cellCount];
         int sum = 0;
 
@@ -184,6 +151,7 @@ public final class ExcelToPdfPdfBoxUtil {
             }
         }
 
+        // 宽度百分比
         float[] colWidthPer = new float[cellCount];
         for (int i = row.getFirstCellNum(); i < cellCount; i++) {
             colWidthPer[i] = (float) colWidths[i] / sum * 100;
@@ -192,7 +160,7 @@ public final class ExcelToPdfPdfBoxUtil {
     }
 
     private static float[] getRowHeight(Sheet sheet) {
-        int rowNum = sheet.getPhysicalNumberOfRows();
+        int rowNum = sheet.getLastRowNum() + 1;
 
         float[] rowHeight = new float[rowNum];
         float sum = 0;
@@ -204,89 +172,70 @@ public final class ExcelToPdfPdfBoxUtil {
             }
         }
 
+        // 高度百分比
         float[] rowHeightPer = new float[rowNum];
         for (int i = sheet.getFirstRowNum(); i < rowNum; i++) {
-            rowHeightPer[i] = (float) rowHeight[i] / sum * 100;
+            rowHeightPer[i] = rowHeight[i] / sum * 100;
         }
         return rowHeightPer;
     }
 
-    public static void excelToPdf(String excelPath, String pdfPath) throws Exception {
-        InputStream in = new FileInputStream(excelPath);
+    public static void excelToPdf(InputStream excelInput, OutputStream pdfOutput) throws Exception {
 
         // 获取工作簿
-        Workbook workbook = getWorkbook(excelPath, in);
+        Workbook workbook = WorkbookFactory.create(excelInput);
 
-
+        // 获取工作表的数量
         int sheetCount = workbook.getNumberOfSheets();
+
         //新建PDF文档
         PDDocument document = new PDDocument();
         for (int i = 0; i < sheetCount; i++) {
+
             // 获取第i张工作表
             Sheet sheet = workbook.getSheetAt(i);
+            // 不处理没有内容的sheet
+            if (sheet.getPhysicalNumberOfRows() == 0) {
+                continue;
+            }
 
-            // 设置页面大小为A4纸
-            PDPage page = new PDPage(PDRectangle.A4);
-            document.addPage(page);
-            PDPageContentStream stream = new PDPageContentStream(document, page);
-            InputStream inputStream = ExcelToPdfPdfBoxUtil.class.getClassLoader().getResourceAsStream("font/STXIHEI.TTF");
-            PDType0Font font = PDType0Font.load(document, inputStream, true);
-            stream.setFont(font, 14);
-            stream.setStrokingColor(Color.BLACK);
+            PDPageContentStream stream = createPageAndContentStream(document);
 
+            // 从classpath中读取字体文件
+            PDType0Font font = loadFontFromClasspath(document);
+
+            // 设置默认字体、字号、颜色
+            setDefaultFontAndColor(stream, font);
+
+            // 获取每列的宽度（从excel宽度转为pdf宽度）
             float[] excelWidths = getColWidth(sheet);
             float[] widths = handleWidths(excelWidths);
 
+            // 获取每行的高度（从excel高度转为pdf高度）
             float[] excelHeights = getRowHeight(sheet);
             float[] heights = handleHeights(excelHeights);
 
+            // 先遍历行，再遍历列，对每一个单元格进行处理
             int colCount = widths.length;
-            for (int rowNum = sheet.getFirstRowNum(); rowNum < sheet.getPhysicalNumberOfRows(); rowNum++) {
+            for (int rowNum = sheet.getFirstRowNum(); rowNum < sheet.getLastRowNum() + 1; rowNum++) {
                 Row row = sheet.getRow(rowNum);
                 if (row != null) {
-                    for (int cellNum = row.getFirstCellNum(); (cellNum < row.getLastCellNum() || cellNum < colCount) && cellNum > -1; cellNum++) {
-                        if (cellNum >= row.getPhysicalNumberOfCells()) {
-                            continue;
-                        }
+                    for (int cellNum = row.getFirstCellNum(); (cellNum < row.getLastCellNum() + 1 || cellNum < colCount) && cellNum > -1; cellNum++) {
                         Cell excelCell = row.getCell(cellNum);
                         // 获取单元格的值
                         String value = getCellValue(excelCell);
 
                         //设置单元格字体
-                        org.apache.poi.ss.usermodel.Font excelFont = getExcelFont(workbook, excelCell, excelPath);
-//                        Font pdFont = new Font(baseFont, excelFont.getFontHeightInPoints(),
-//                                excelFont.getBold() ? Font.BOLD : Font.NORMAL, BaseColor.BLACK);
+                        org.apache.poi.ss.usermodel.Font excelFont = getExcelFont(workbook, excelCell);
 
-
-//                         处理图片
+                        // 单独处理图片
                         List<PicturesInfo> infos = PoiExtend.getAllPictureInfos(sheet, rowNum, rowNum, cellNum, cellNum, false);
-                        if (!infos.isEmpty()) {
-                            PicturesInfo info = infos.get(0);
+                        drawImage(document, stream, widths, heights, rowNum, cellNum, infos);
 
-                            float x = 29.76f;
-                            float y = PAGE_HEIGHT - 15f;
-                            for (int j = 0; j < cellNum; j++) {
-                                x += widths[j];
-                            }
-
-                            float width = 0;
-                            for (int j = cellNum; j < cellNum + info.getMaxCol() - info.getMinCol() + 1; j++) {
-                                width += widths[j];
-                            }
-
-                            for (int j = 0; j < rowNum; j++) {
-                                y -= heights[j];
-                            }
-                            float height = 0;
-                            for (int j = rowNum; j < rowNum + info.getMaxRow() - info.getMinRow() + 1; j++) {
-                                height += heights[j];
-                            }
-
-                            stream.drawImage(PDImageXObject.createFromByteArray(document, info.getPictureData(), null), x, y - height, width, height);
-                        }
-
-
+                        // 判断是否有边框
                         int border = getBorder(Objects.requireNonNull(excelCell));
+
+                        // 判断是否为合并单元格，分别处理
                         if (isMergedRegion(sheet, rowNum, cellNum)) {
                             int[] span = getMergedSpan(sheet, rowNum, cellNum);
                             //忽略合并过的单元格
@@ -299,6 +248,8 @@ public final class ExcelToPdfPdfBoxUtil {
                             PdfUtil.drawString(font, stream, excelFont, widths, heights, rowNum, cellNum, span[0], span[1], value);
                             cellNum = cellNum + span[1] - 1;
                         } else {
+
+                            // 非合并的单元格直接绘制
                             PdfUtil.drawRect(stream, widths, heights, rowNum, cellNum, 1, 1, border);
                             PdfUtil.drawString(font, stream, excelFont, widths, heights, rowNum, cellNum, 1, 1, value);
                         }
@@ -309,9 +260,72 @@ public final class ExcelToPdfPdfBoxUtil {
             stream.close();
         }
 
-        document.save(pdfPath);
+        document.save(pdfOutput);
         document.close();
 
+    }
+
+    private static void setDefaultFontAndColor(PDPageContentStream stream, PDType0Font font) throws IOException {
+        stream.setFont(font, 14);
+        stream.setStrokingColor(Color.BLACK);
+    }
+
+    private static PDType0Font loadFontFromClasspath(PDDocument document) throws IOException {
+        InputStream inputStream = ExcelToPdfPdfBoxUtil.class.getClassLoader().getResourceAsStream("font/STXIHEI.TTF");
+        return PDType0Font.load(document, inputStream, true);
+    }
+
+    private static PDPageContentStream createPageAndContentStream(PDDocument document) throws IOException {
+        // 设置页面大小为A4纸
+        PDPage page = new PDPage(PDRectangle.A4);
+
+        // 将页面加入文档中
+        document.addPage(page);
+
+        // 创建处理pdf内容的流
+        return new PDPageContentStream(document, page);
+    }
+
+    private static void drawImage(PDDocument document, PDPageContentStream stream, float[] widths, float[] heights, int rowNum, int cellNum, List<PicturesInfo> infos) throws IOException {
+        if (!infos.isEmpty()) {
+            // 目前只处理第一张图片
+            PicturesInfo info = infos.get(0);
+
+            // 左上角坐标
+            float x = 29.76f;
+            float y = PAGE_HEIGHT - 15f;
+
+            // 图片起始X坐标
+            for (int j = 0; j < cellNum; j++) {
+                x += widths[j];
+            }
+
+            // 图片宽度
+            float width = 0;
+            for (int j = cellNum; j < cellNum + info.getMaxCol() - info.getMinCol() + 1; j++) {
+                width += widths[j];
+            }
+
+            // 图片起始Y坐标
+            for (int j = 0; j < rowNum; j++) {
+                y -= heights[j];
+            }
+
+            // 图片高度
+            float height = 0;
+            for (int j = rowNum; j < rowNum + info.getMaxRow() - info.getMinRow() + 1; j++) {
+                height += heights[j];
+            }
+
+            // 绘制图片
+            stream.drawImage(PDImageXObject.createFromByteArray(document, info.getPictureData(), null), x, y - height, width, height);
+        }
+    }
+
+    public static void excelToPdf(String excelPath, String pdfPath) throws Exception {
+        InputStream in = new FileInputStream(excelPath);
+        OutputStream out = new FileOutputStream(pdfPath);
+        excelToPdf(in, out);
     }
 
     private static float[] handleHeights(float[] excelHeights) {
@@ -325,7 +339,7 @@ public final class ExcelToPdfPdfBoxUtil {
     private static float[] handleWidths(float[] excelWidths) {
         float[] widths = new float[excelWidths.length];
         for (int i = 0; i < excelWidths.length; i++) {
-            widths[i] = excelWidths[i] / 100 * PAGE_WIDTH * 0.94f;
+            widths[i] = excelWidths[i] / 100 * PAGE_WIDTH * CONTENT_WIDTH_PERCENT;
         }
         return widths;
     }
