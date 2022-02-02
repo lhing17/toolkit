73c84c64ce0c8a1f475317060721400ddaea1d25
lhing17 <lhing17@163.com>
2022-02-02 23:44:48 +0800

    feat: clj代码片段。

--- clj/src/clj/snippets.clj
+++ clj/src/clj/snippets.clj
@@ -1,5 +1,6 @@
 (ns clj.snippets
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:import (java.util Properties)))
 
 (defn underline->camel [name]
   "下划线转驼峰"
@@ -19,3 +20,34 @@
        (map-indexed #(when (pred %2) %))
        (filter identity)                                    ;; 去掉nil
        (first)))
+
+(defn revrot [strng sz]
+  "反转或翻转"
+  (letfn [(cubic [n] (* n n n))
+          (rev-or-rot [coll]
+            (let [sum (apply + (map (comp cubic read-string str) coll))]
+              (if (even? sum) (reverse coll) (concat (rest coll) [(first coll)]))))]
+    (if (or (<= sz 0)
+            (> sz (count strng))
+            (empty? strng))
+      ""
+      (->> strng
+           (partition sz)
+           (map (comp #(apply str %) rev-or-rot))
+           (apply str)))))
+
+
+(defn- ^Properties as-properties
+  "将map转成java.util.Properties实例。
+  Convert any seq of pairs to a `java.util.Properties` instance."
+  [m]
+  (let [p (Properties.)]
+    (doseq [[k v] m]
+      (.setProperty p (name k) (str v)))
+    p))
+
+(defn- main [& args]
+  (print (revrot "733049910872815764", 5))
+  )
+
+(main)
